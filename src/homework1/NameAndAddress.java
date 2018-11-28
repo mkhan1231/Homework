daa2f3935bbae8196ac5eafc14cba9c0276fb99e
mkhan1231 <mahmud8118@gmail.com>
2018-11-27 22:22:41 -0500

    Hello Work

---
+++ src/homework1/NameAndAddress.java
@@ -0,0 +1,20 @@
+package homework1;
+
+public class NameAndAddress {
+	
+
+
+	public static void main(String[] args) {
+		 String firstName="Mahmud";
+		 String lastName="Khan";
+		 String studentID="3788NY";
+		 String address="161-08 85th Ave, Jamaica, NY 11432";
+		
+		
+		System.out.println("My name is "+ firstName+" "+lastName);
+		System.out.println("My PnT ID is"+" "+ studentID);		
+		System.out.println("My address is "+ address);
+	}
+
+}
+
